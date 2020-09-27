6611aab07e1b61643b2752ffbe19895078ee5b42
auddnjs2008 <46766443+auddnjs2008@users.noreply.github.com>
2020-09-27 21:59:30 +0900

    대화창에서 친구초대 모달창 디자인

--- assets/js/chatAddFriend.js
+++ assets/js/chatAddFriend.js
@@ -1,6 +1,11 @@
+const chatRoom = document.querySelector(".chatRoom");
+const chatInputForm = document.querySelector(".chatFormWrapper");
+const formContainer = document.querySelector(".chatFriendsForm");
 const chatAddForm = document.querySelector(".chatFriends__form");
 const chatAddInput = chatAddForm ? chatAddForm.querySelector("input") : "";
 const chatAddUl = document.querySelector(".plusFriends");
+const searchFriend = document.querySelector(".chatFriendsPlus");
+const closeBtn = document.querySelector(".closeBtn");
 
 const foundFriend = (findValue) => {
   if (findValue.length !== 0) {
@@ -12,7 +17,7 @@ const foundFriend = (findValue) => {
         </div> 
         <i class="fas fa-plus-circle chatAddBtn"></i>
         `
-      : `<div class="chatAddFriend"> <i class="fas fa-user-alt"></i>
+      : `<div class="chatAddFriend"> <div class="icon"><i class="fas fa-user-alt"></i></div>
         <div class="chatAddName"/>${findValue[0].name}</div> </div>
         <i class="fas fa-plus-circle chatAddBtn"></i>
         `;
@@ -34,8 +39,35 @@ const handleChatAdd = (e) => {
   chatAddSocket(findValue);
 };
 
+const handleAddClick = () => {
+  formContainer.style.display = "block";
+  formContainer.classList.remove("modalCloseAnimation");
+  formContainer.classList.add("modalAnimation");
+  chatAddInput.classList.remove("inputCloseModal");
+  chatAddInput.classList.add("inputModal");
+
+  chatRoom.style.opacity = "0.1";
+  chatInputForm.style.opacity = "0.1";
+};
+
+const handleCloseClick = () => {
+  formContainer.classList.remove("modalAnimation");
+  formContainer.classList.add("modalCloseAnimation");
+  //formContainer.style.display = "none";
+  chatAddInput.classList.remove("inputModal");
+  chatAddInput.classList.add("inputCloseModal");
+
+  chatRoom.style.opacity = "1";
+  chatInputForm.style.opacity = "1";
+  if (chatAddUl.firstElementChilds)
+    chatAddUl.removeChild(chatAddUl.firstElementChild);
+  chatAddUl.innerText = "";
+};
+
 const init = () => {
-  chatAddForm.addEventListener("submit", handleChatAdd);
+  if (chatAddForm) chatAddForm.addEventListener("submit", handleChatAdd);
+  if (searchFriend) searchFriend.addEventListener("click", handleAddClick);
+  if (closeBtn) closeBtn.addEventListener("click", handleCloseClick);
 };
 
 init();
