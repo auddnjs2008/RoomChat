4a8f6968025ae113dd7468afe3319fb95340dc5a
auddnjs2008 <46766443+auddnjs2008@users.noreply.github.com>
2020-10-07 21:23:24 +0900

    amazon multer s3 , mongo atlas setting완료, 빌드중

---
+++ src/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL_PROD, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("connected to Db");
+const handleError = (error) => console.log(`Error on DB Connection: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
---
+++ src/localMiddleware.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+import app from "./server";
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+  region: "ap-northeast-1",
+});
+
+const Images = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "wewechat/images",
+  }),
+});
+
+export const roomImageUpload = Images.single("roombackground");
+export const imageUpload = Images.fields([
+  { name: "avatar", maxCount: 1 },
+  { name: "background", maxCount: 1 },
+]);
+
+export const localMiddleware = (req, res, next) => {
+  res.locals.loggedUser = req.user || null;
+  if (req.user) app.locals.user = req.user.id || null;
+  next();
+};
