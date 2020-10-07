aab94f8174a2104fd1cc794cfbb9e70cd66e3b01
auddnjs2008 <46766443+auddnjs2008@users.noreply.github.com>
2020-10-08 00:13:50 +0900

    change

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use("/static", express.static(path.join(__dirname, "static")));
 
-app.use(cookieParser());
+app.use(cookieParser("secret"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
